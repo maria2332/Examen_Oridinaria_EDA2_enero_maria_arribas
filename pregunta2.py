bccc697ea63003fd5c1888656f6eca610ae39f60
maria2332 <maria.ballesteros2004@gmail.com>
2023-05-25 08:43:41 +0200

    cambio

--- pregunta2.py
+++ pregunta2.py
@@ -2,7 +2,7 @@
 Crea una clase llamada Pokemon.py que tenga los atributos nombre y tipo. Crea el constructor de la clase. Añadir en el constructor un print para informar de que el Pokemon se ha creado con éxito. Crear un método llamado clasificacion que clasifique a los Pokemon según su tipo de la siguiente manera:
 los PS, el Ataque, la Defensa, el Ataque Especial, la Defensa Especial y la Velocidad.
 
-Experimentación (0,5 Puntos)
+Experimentación
 Crea una lista con un numero arbitrario de objetos tipo Pokemon. Recorre los elementos de la lista, y prueba a ejecutar el método clasificacion de cada objeto que has creado.
 """
 
