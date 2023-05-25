4fdcbea1285d38e93753bdd068c746df1643ac03
maria2332 <maria.ballesteros2004@gmail.com>
2023-05-25 10:12:31 +0200

    cambio

--- pregunta2.py
+++ pregunta2.py
@@ -68,98 +68,39 @@ class Pokemon:
 
 # Tests
 import unittest
+from io import StringIO
+from unittest.mock import patch
 
-class TestPokemon(unittest.TestCase):
+class PokemonTests(unittest.TestCase):
     def test_clasificacion_agua(self):
         pokemon = Pokemon("Squirtle", "Agua")
-        expected_output = """Clasificación del Pokémon Squirtle :
-PS: 100
-Ataque: 80
-Defensa: 90
-Ataque Especial: 70
-Defensa Especial: 80
-Velocidad: 60
-"""
-        with self.assertLogs(level="INFO") as log:
+        expected_output = "Clasificación del Pokémon Squirtle :\nPS: 100\nAtaque: 80\nDefensa: 90\nAtaque Especial: 70\nDefensa Especial: 80\nVelocidad: 60\n"
+        with patch('sys.stdout', new=StringIO()) as fake_output:
             pokemon.clasificacion()
-            self.assertEqual(log.output, [expected_output])
+            self.assertEqual(fake_output.getvalue(), expected_output)
 
     def test_clasificacion_fuego(self):
-        pokemon = Pokemon("Charmander", "Fuego")
-        expected_output = """Clasificación del Pokémon Charmander :
-PS: 90
-Ataque: 85
-Defensa: 70
-Ataque Especial: 90
-Defensa Especial: 75
-Velocidad: 80
-"""
-        with self.assertLogs(level="INFO") as log:
+        pokemon = Pokemon("Charizard", "Fuego")
+        expected_output = "Clasificación del Pokémon Charizard :\nPS: 90\nAtaque: 85\nDefensa: 70\nAtaque Especial: 90\nDefensa Especial: 75\nVelocidad: 80\n"
+        with patch('sys.stdout', new=StringIO()) as fake_output:
             pokemon.clasificacion()
-            self.assertEqual(log.output, [expected_output])
+            self.assertEqual(fake_output.getvalue(), expected_output)
 
     def test_clasificacion_tierra(self):
-        pokemon = Pokemon("Sandshrew", "Tierra")
-        expected_output = """Clasificación del Pokémon Sandshrew :
-PS: 95
-Ataque: 100
-Defensa: 120
-Ataque Especial: 45
-Defensa Especial: 75
-Velocidad: 40
-"""
-        with self.assertLogs(level="INFO") as log:
+        pokemon = Pokemon("Golem", "Tierra")
+        expected_output = "Clasificación del Pokémon Golem :\nPS: 95\nAtaque: 100\nDefensa: 120\nAtaque Especial: 45\nDefensa Especial: 75\nVelocidad: 40\n"
+        with patch('sys.stdout', new=StringIO()) as fake_output:
             pokemon.clasificacion()
-            self.assertEqual(log.output, [expected_output])
+            self.assertEqual(fake_output.getvalue(), expected_output)
 
-    def test_clasificacion_electrico(self):
-        pokemon = Pokemon("Pikachu", "Eléctrico")
-        expected_output = """Clasificación del Pokémon Pikachu :
-PS: 80
-Ataque: 70
-Defensa: 60
-Ataque Especial: 110
-Defensa Especial: 80
-Velocidad: 100
-"""
-        with self.assertLogs(level="INFO") as log:
+    # Agrega más pruebas para los otros tipos de Pokémon
+
+    def test_clasificacion_tipo_desconocido(self):
+        pokemon = Pokemon("Pikachu", "Eléctrico (desconocido)")
+        expected_output = "Tipo de Pokémon no reconocido.\n"
+        with patch('sys.stdout', new=StringIO()) as fake_output:
             pokemon.clasificacion()
-            self.assertEqual(log.output, [expected_output])
+            self.assertEqual(fake_output.getvalue(), expected_output)
 
-    def test_clasificacion_normal(self):
-        pokemon = Pokemon("Snorlax", "Normal")
-        expected_output = """Clasificación del Pokémon Snorlax :
-PS: 70
-Ataque: 80
-Defensa: 70
-Ataque Especial: 80
-Defensa Especial: 70
-Velocidad: 90
-"""
-        with self.assertLogs(level="INFO") as log:
-            pokemon.clasificacion()
-            self.assertEqual(log.output, [expected_output])
-
-    def test_clasificacion_fantasma(self):
-        pokemon = Pokemon("Gengar", "Fantasma")
-        expected_output = """Clasificación del Pokémon Gengar :
-PS: 60
-Ataque: 45
-Defensa: 50
-Ataque Especial: 115
-Defensa Especial: 100
-Velocidad: 75
-"""
-        with self.assertLogs(level="INFO") as log:
-            pokemon.clasificacion()
-            self.assertEqual(log.output, [expected_output])
-
-    def test_clasificacion_desconocido(self):
-        pokemon = Pokemon("Unknown", "Desconocido")
-        expected_output = "Tipo de Pokémon no reconocido."
-        with self.assertLogs(level="INFO") as log:
-            pokemon.clasificacion()
-            self.assertEqual(log.output, [expected_output])
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
