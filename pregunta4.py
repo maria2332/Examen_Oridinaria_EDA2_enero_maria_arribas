bf973e47f7fa97e0dd166b644e7c819312c4f2d8
maria2332 <maria.ballesteros2004@gmail.com>
2023-05-25 08:51:00 +0200

    cambio

--- pregunta4.py
+++ pregunta4.py
@@ -5,3 +5,47 @@ Crea el método str para visualizar por pantalla la información de las Pokeball
 Experimentación 
 Crea algunas Pokeballs. Prueba a mostrar los datos de algunas Pokeballs ordenadas por su fecha de fabricación y a modificar algún valor, por ejemplo, prueba a modificar el precio de una de las Pokeballs.
 """
+
+class Pokeball:
+    def __init__(self, peso, nombre, precio, fecha_fabricacion):
+        self.peso = peso
+        self.nombre = nombre
+        self.precio = precio
+        self.fecha_fabricacion = fecha_fabricacion
+        print("¡Se ha creado exitosamente una Pokeball llamada", self.nombre, "!")
+
+    def __str__(self):
+        return f"Información de la Pokeball:\nNombre: {self.nombre}\nPeso: {self.peso} gramos\nPrecio: ${self.precio}\nFecha de fabricación: {self.fecha_fabricacion}"
+
+# Ejemplo de uso
+pokeball1 = Pokeball(100, "Superball", 200, "10/05/2023")
+print(pokeball1)
+
+
+from datetime import datetime
+
+class Pokeball:
+    def __init__(self, peso, nombre, precio, fecha_fabricacion):
+        self.peso = peso
+        self.nombre = nombre
+        self.precio = precio
+        self.fecha_fabricacion = fecha_fabricacion
+        print("¡Se ha creado exitosamente una Pokeball llamada", self.nombre, "!")
+
+    def __str__(self):
+        return f"Información de la Pokeball:\nNombre: {self.nombre}\nPeso: {self.peso} gramos\nPrecio: ${self.precio}\nFecha de fabricación: {self.fecha_fabricacion}"
+
+# Crear algunas Pokeballs
+pokeball1 = Pokeball(100, "Superball", 200, datetime(2023, 5, 10))
+pokeball2 = Pokeball(80, "Ultraball", 300, datetime(2023, 4, 20))
+pokeball3 = Pokeball(120, "Masterball", 500, datetime(2023, 6, 5))
+
+# Mostrar los datos de las Pokeballs ordenadas por fecha de fabricación
+pokeballs = [pokeball1, pokeball2, pokeball3]
+pokeballs_sorted = sorted(pokeballs, key=lambda x: x.fecha_fabricacion)
+for pokeball in pokeballs_sorted:
+    print(pokeball)
+
+# Modificar el precio de una Pokeball
+pokeball2.precio = 350
+print("Precio modificado de la Ultraball:", pokeball2.precio)
