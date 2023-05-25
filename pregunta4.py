a9312e492276e1ffb8074f71459d4f8a9dd6030f
maria2332 <maria.ballesteros2004@gmail.com>
2023-05-25 08:49:33 +0200

    cambio

---
+++ pregunta4.py
@@ -0,0 +1,7 @@
+"""
+Crea una clase llamada Pokeball.py que tenga los atributos peso, nombre, precio y fecha de fabricación. Crea el constructor de la clase. Añade en el constructor un print para informar de que la Pokeball se ha creado con éxito.
+Crea el método str para visualizar por pantalla la información de las Pokeballs.
+
+Experimentación 
+Crea algunas Pokeballs. Prueba a mostrar los datos de algunas Pokeballs ordenadas por su fecha de fabricación y a modificar algún valor, por ejemplo, prueba a modificar el precio de una de las Pokeballs.
+"""
