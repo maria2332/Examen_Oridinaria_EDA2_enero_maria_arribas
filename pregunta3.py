19afb76f7eb04753b23463df7ac3438cd3c67792
maria2332 <maria.ballesteros2004@gmail.com>
2023-05-25 10:08:45 +0200

    cambio

--- pregunta3.py
+++ pregunta3.py
@@ -37,46 +37,36 @@ class Pokemon:
 
 # Test
 
-def test_clasificacion_agua():
-    pikachu = Pokemon("Pikachu", "Agua")
-    expected_output = "Clasificación del Pokémon Pikachu:\nPS: 100\nAtaque: 80\nDefensa: 90\nAtaque Especial: 70\nDefensa Especial: 80\nVelocidad: 60"
-    assert pikachu.clasificacion() == expected_output
+import unittest
 
-def test_clasificacion_fuego():
-    charizard = Pokemon("Charizard", "Fuego")
-    expected_output = "Clasificación del Pokémon Charizard:\nPS: 90\nAtaque: 85\nDefensa: 70\nAtaque Especial: 90\nDefensa Especial: 75\nVelocidad: 80"
-    assert charizard.clasificacion() == expected_output
+class PokemonTests(unittest.TestCase):
+    def test_clasificacion_agua(self):
+        pokemon = Pokemon("Squirtle", "Agua")
+        clasificacion_esperada = "Clasificación del Pokémon Squirtle:\nPS: 100\nAtaque: 80\nDefensa: 90\nAtaque Especial: 70\nDefensa Especial: 80\nVelocidad: 60"
+        self.assertEqual(pokemon.clasificacion(), clasificacion_esperada)
 
-def test_clasificacion_tierra():
-    sandslash = Pokemon("Sandslash", "Tierra")
-    expected_output = "Clasificación del Pokémon Sandslash:\nPS: 95\nAtaque: 100\nDefensa: 120\nAtaque Especial: 45\nDefensa Especial: 75\nVelocidad: 40"
-    assert sandslash.clasificacion() == expected_output
+    def test_clasificacion_fuego(self):
+        pokemon = Pokemon("Charizard", "Fuego")
+        clasificacion_esperada = "Clasificación del Pokémon Charizard:\nPS: 90\nAtaque: 85\nDefensa: 70\nAtaque Especial: 90\nDefensa Especial: 75\nVelocidad: 80"
+        self.assertEqual(pokemon.clasificacion(), clasificacion_esperada)
 
-def test_clasificacion_electrico():
-    pikachu = Pokemon("Pikachu", "Eléctrico")
-    expected_output = "Clasificación del Pokémon Pikachu:\nPS: 80\nAtaque: 70\nDefensa: 60\nAtaque Especial: 110\nDefensa Especial: 80\nVelocidad: 100"
-    assert pikachu.clasificacion() == expected_output
+    def test_clasificacion_tierra(self):
+        pokemon = Pokemon("Golem", "Tierra")
+        clasificacion_esperada = "Clasificación del Pokémon Golem:\nPS: 95\nAtaque: 100\nDefensa: 120\nAtaque Especial: 45\nDefensa Especial: 75\nVelocidad: 40"
+        self.assertEqual(pokemon.clasificacion(), clasificacion_esperada)
 
-def test_clasificacion_normal():
-    normal_pokemon = Pokemon("Normalmon", "Normal")
-    expected_output = "Clasificación del Pokémon Normalmon:\nPS: 70\nAtaque: 80\nDefensa: 70\nAtaque Especial: 80\nDefensa Especial: 70\nVelocidad: 90"
-    assert normal_pokemon.clasificacion() == expected_output
+    # Agrega más pruebas para los otros tipos de Pokémon
 
-def test_clasificacion_fantasma():
-    gengar = Pokemon("Gengar", "Fantasma")
-    expected_output = "Clasificación del Pokémon Gengar:\nPS: 60\nAtaque: 45\nDefensa: 50\nAtaque Especial: 115\nDefensa Especial: 100\nVelocidad: 75"
-    assert gengar.clasificacion() == expected_output
+    def test_clasificacion_tipo_desconocido(self):
+        pokemon = Pokemon("Pikachu", "Eléctrico (desconocido)")
+        clasificacion_esperada = "Tipo de Pokémon no reconocido."
+        self.assertEqual(pokemon.clasificacion(), clasificacion_esperada)
 
-def test_clasificacion_desconocido():
-    unknown_pokemon = Pokemon("Unknownmon", "Desconocido")
-    expected_output = "Tipo de Pokémon no reconocido."
-    assert unknown_pokemon.clasificacion() == expected_output
+    def test_str(self):
+        pokemon = Pokemon("Bulbasaur", "Planta")
+        str_esperado = "Nombre: Bulbasaur\nTipo: Planta\n"
+        self.assertEqual(str(pokemon), str_esperado)
 
-# Ejecutar los tests
-test_clasificacion_agua()
-test_clasificacion_fuego()
-test_clasificacion_tierra()
-test_clasificacion_electrico()
-test_clasificacion_normal()
-test_clasificacion_fantasma()
-test_clasificacion_desconocido()
+if __name__ == '__main__':
+    unittest.main()
+
